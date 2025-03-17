394e19c7a7f8059d9baf4916c44f1e46635044fc
Giovanni-inno-eng <giovanni.piccolo@eng.it>
2025-03-17 11:00:37 +0000

    task02/ended exercise 3a

---
+++ assignment02_ComputationalHelloWorld/3a_matmul.jl
@@ -0,0 +1,28 @@
+function compute_product(A, B)
+    return A .* B
+end
+
+# parameters
+N = [10, 100, 10_000]
+
+# loops over the different values of N
+for n in N 
+    time_result = @timed begin
+
+        # matrix definition and multiplication
+        A = 3 .* ones(n, n)
+        B = 7.1 .* ones(n, n)
+        C = compute_product(A, B)
+
+        expected_value = 3 * 7.1  
+
+        # test the result
+        if all(C .== expected_value) 
+            println("N = $n: all the elements of d are equal to $(expected_value)")
+        else
+            println("N = $n: NOT all the elements of d are equal to $(expected_value)")
+        end
+    end
+
+    println("Execution time: $(time_result.time) s\n")
+end
